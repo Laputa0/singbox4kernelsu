841ae5b388d5b0b6c68675375010a798792c229a
Laputa0 <1737261850wang@gmail.com>
2024-09-21 19:55:18 +0800

    update.

--- boot-completed.sh
+++ boot-completed.sh
@@ -14,6 +14,13 @@ cat $rule_sets_file | grep RULE_SETS_PATH
 if [[ $? -eq 0 ]]; then
     sed -i "s#RULE_SETS_PATH#${RULES_DIR}#g" $rule_sets_file
 fi
+# modify sing-box log path
+box_log_file=$CONF_DIR/log.json
+cat $box_log_file | grep BOX_LOG_PATH
+if [[ $? -eq 0 ]]; then
+    sed -i "s#BOX_LOG_PATH#${LOG_DIR}/sing-box.log#g" $box_log_file
+fi
+
 
 while true; do
 sleep 5
@@ -21,8 +28,8 @@ date
 pidof sing-box
 service_status=$?
 if [[ ! -e "${MODDIR}/disable" ]] && [[ $service_status -ne 0 ]]; then
-    $BIN_DIR/sing-box -C $CONF_DIR -D $RUN_DIR run &> $LOG_DIR/sing-box.log &
-elif [[ $service_status -eq 0 ]]; then
+    $BIN_DIR/sing-box -C $CONF_DIR -D $RUN_DIR run &
+elif [[ -e "${MODDIR}/disable" ]] && [[ $service_status -eq 0 ]]; then
     (pkill sing-box && date) &> $LOG_DIR/pkill.log
 fi
 done &> $LOG_DIR/loop.log
